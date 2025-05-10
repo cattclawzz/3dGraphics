39a6b17aacd72235c48882650a6c69f30747dbf2
cattclawzz <anakinedwards9@gmail.com>
2025-05-10 19:51:40 +0100

    saving current code, issue with drawing z2 of cube

---
+++ mainCode.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+
+'''
+(formulas from from @MathMathMath on scratch)
+
+screen x = focalLength * x/z
+screen y = focalLength * y/z
+
+'''
+
+window = tk.Tk()
+canX,canY,canZ = 500,500,100
+canvas = tk.Canvas(window, width=canX, height=canY, bg='light grey')
+canvas.pack()
+
+def drawSquare(x1,y1, x2,y2):
+    points = [[x1,y1], [x2,y1], [x2,y2], [x1,y2]]
+    for i in range(4):
+        canvas.create_line(*points[i], *points[(i+1)%4], fill= 'red')
+
+
+def plotPoint(x, y, z, focalLength):
+    return [focalLength*(x/z), focalLength*(y/z)]
+
+def drawLine(x1,y1,z1, x2,y2,z2, focalLength, colour='black'):
+    canvas.create_line(*plotPoint(x1,y1,z1,focalLength), *plotPoint(x2,y2,z2,focalLength), fill= colour)
+
+def drawCube(x1,y1,z1, x2,y2,z2, focalLength):
+    points = [[x1,y1], [x2,y1], [x2,y2], [x1,y2]]
+    for i in range(4):
+        drawLine(*points[i],z1, *points[(i+1)%4],z1, focalLength, 'red')
+    for i in range(4):
+        drawLine(*points[i],z2, *points[(i+1)%4],z2, focalLength, 'blue')
+
+def render():
+    canvas.delete("all")
+
+    #drawSquare(100,100, 400,400)
+    drawCube(100,100,100, 400,400,120, canZ)
+
+    window.after(16, render)
+
+render()
+window.mainloop()

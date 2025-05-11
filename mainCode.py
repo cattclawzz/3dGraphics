a83cd66bfe967eab77ca93775857eaa579eb7c77
cattclawzz <anakinedwards9@gmail.com>
2025-05-11 14:42:33 +0100

    added drawGrid(). it does NOT work

--- mainCode.py
+++ mainCode.py
@@ -1,13 +1,5 @@
 import tkinter as tk
 
-'''
-(formulas from from @MathMathMath on scratch)
-
-screen x = focalLength * x/z
-screen y = focalLength * y/z
-
-'''
-
 window = tk.Tk()
 canX,canY,canZ = 500,500,100
 canvas = tk.Canvas(window, width=canX, height=canY, bg='light grey')
@@ -20,9 +12,15 @@ def drawSquare(x1,y1, x2,y2):
 
 
 def plotPoint(x, y, z, focalLength):
+    '''
+    screen x = focalLength * x/z
+    screen y = focalLength * y/z
+
+    The original formulas worked assuming (0,0) is the center of the plane
+    removing can before and after running the formula adjusts it for the tkinter canvas
+    '''
     def plotCoord(w, can): #defining a function within a function? functionseption
-        return (focalLength * ((w-(can/2)) / z)) + (can/2) #The original formulas worked assuming (0,0) is the center of the plane
-                                                           #removing can before and after running the formula adjusts it for the tkinter canvas
+        return (focalLength * ((w-(can/2)) / z)) + (can/2)
     return[plotCoord(x,canX), plotCoord(y,canY)]
 
 def drawLine(x1,y1,z1, x2,y2,z2, focalLength, colour='black'):
@@ -37,11 +35,19 @@ def drawCube(x1,y1,z1, x2,y2,z2, focalLength):
     for i in points:
         drawLine(*i,z1, *i,z2, focalLength)
 
+def drawGrid():
+    for j in range(100, 400, 10):
+        for i in range(0,canX, 50):
+            drawLine(i,0,j, i,canY,j, canZ)
+        for i in range(0,canY, 50):
+            drawLine(0,i,j, canX,i,j, canZ)
+
 def render():
     canvas.delete("all")
 
     #drawSquare(100,100, 400,400)
-    drawCube(100,100,100, 400,400,150, canZ)
+    #drawCube(250,250,300, 450,450,200, canZ)
+    drawGrid()
 
     window.after(16, render)
 
